355ea693061d0b4cea543a30605abcb512f781f4
Leksyking <gbemilekeogundipe@gmail.com>
2023-07-29 00:10:33 +0100

    continue with authentication

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
-require("dotenv").config();
-
-require("express-async-errors");
-
+import "dotenv/config";
+import "express-async-errors";
 const express = require("express");
 
 const app = express();
-
 const cors = require("cors");
-
 const fileupload = require("express-fileupload");
+const session = require("express-session");
+const passport = require("passport");
+const MongoStore = require("connect-mongo");
+
+const tenDays = 1000 * 60 * 60 * 24 * 30;
 
 // Use Express Middleware
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileupload({ useTempFiles: true }));
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        maxAge: new Date(Date.now() + tenDays),
+        store: MongoStore.create({
+            mongoUrl: process.env.DB_CONNECTION_DEV,
+            collectionName: "sessions",
+        }),
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./controllers/user.controller/passportControllers");
 
 // Declare Routes
 // app.use("/api/v1/contact", require("./routes/contact/index"));
 
 app.use("/", (req, res) => {
-  res.send("Welcome to OAU Car-Pooling...");
+    res.send("Welcome to OAU Car-Pooling...");
 });
 app.use(require("./middlewares/notFoundMiddleware"));
 app.use(require("./middlewares/errorHandlerMiddleware"));
@@ -29,13 +46,13 @@ const connectDB = require("./db/database");
 const PORT = process.env.PORT || 4400;
 
 const start = async () => {
-  try {
-    await connectDB();
-    console.log("Successfully connected to the database");
-    app.listen(PORT);
-    console.log(`Listening to ${PORT}:${PORT}`);
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        await connectDB();
+        console.log("Successfully connected to the database");
+        app.listen(PORT);
+        console.log(`Listening to ${PORT}:${PORT}`);
+    } catch (error) {
+        console.log(error);
+    }
 };
 start();
---
+++ controllers/user.controller/passportController.js
@@ -0,0 +1,57 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("../../models/user.model/user");
+const customApiError = require("../../errors");
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: process.env.GOOGLE_CALLBACK_URL,
+        },
+        async function (accessToken, refreshToken, profile, cb) {
+            const { email, given_name, family_name, name } = profile._json;
+
+            const userDetails = {
+                fullName: name,
+                googleId: profile.id,
+                email,
+                firstName: given_name,
+                lastName: family_name,
+                isVerified: {
+                    email: true,
+                },
+                verified: new Date(Date.now()),
+            };
+            //check if the email is in the db
+            const user = await User.findOne({ email, googleId: profile.id });
+            if (user) {
+                return cb(null, user);
+            } else {
+                const existingUser = await User.findOne({ email });
+                if (existingUser)
+                    throw new customApiError.BadRequestError(
+                        "User with this email already exists"
+                    );
+
+                const newUser = await User.create(userDetails);
+                return cb(null, newUser);
+            }
+        }
+    )
+);
+
+passport.serializeUser(function (user, cb) {
+    process.nextTick(function () {
+        cb(null, user);
+    });
+});
+
+passport.deserializeUser(function (user, cb) {
+    process.nextTick(function () {
+        return cb(null, user);
+    });
+});
+
+module.exports = passport;
