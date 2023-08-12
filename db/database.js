b34291804442019613d95750392513f214491def
Leksyking <gbemilekeogundipe@gmail.com>
2023-08-12 14:20:15 +0100

    fix mongodb connection error

--- db/database.js
+++ db/database.js
@@ -1,8 +1,7 @@
-const mongoose = require("mongoose");
+const { set, connect } = require("mongoose");
 
 const connectDB = async () => {
-    mongoose.set("strictQuery", true);
-    return mongoose.connect("mongodb+srv://leksyking:WxcrdxZLvlkfUT5h@cluster0.3mamy9w.mongodb.net/carpooling?retryWrites=true&w=majority");
+    set("strictQuery", true);
+    return connect(process.env.NODE_ENV == "local" ? process.env.DB_CONNECTION_DEV : process.env.DB_CONNECTION_PROD);
 };
 module.exports = connectDB;
-// process.env.NODE_ENV == "local" ? process.env.DB_CONNECTION_DEV : process.env.DB_CONNECTION_PROD);
